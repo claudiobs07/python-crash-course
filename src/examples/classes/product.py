1cd74d088798ef48bb2bfa0274e7a6540502c44f
Claudio Santos <claudiobs07@users.noreply.github.com>
2017-10-08 16:59:45 -0300

    add: beginning presentation

---
+++ src/examples/classes/product.py
@@ -0,0 +1,8 @@
+class Product():
+
+    def __init__(self, name, price, brand, inventory):
+        self.name = name
+        self.price = price
+        self.brand = brand
+        self.inventory = inventory
+
